86b4249ea0d8edca2dd180581fe4fcaee6ab54bf
shinker1002 <shinker1002@naver.com>
2023-10-06 15:35:57 +0900

    Feat: tailwind css 적용

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,6 +4,7 @@ import { Outlet } from "react-router-dom";
 const Layout: React.FC = () => {
   return (
     <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <header style={{ background: "pink", padding: 16, fontSize: 24 }}>
         header
       </header>
